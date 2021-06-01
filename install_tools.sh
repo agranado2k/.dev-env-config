ab258de78439987dcd8c6d62a7d9adde9929cfd3
Arthur Granado <arthur.granado@snyk.io>
2021-06-01 13:44:51 +0100

    feat: include dracula theme for .zsh

--- install_tools.sh
+++ install_tools.sh
@@ -14,13 +14,11 @@ if [[ $IS_BREW_INSTALLED ]]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   echo "Homebrew installed."
 else
-  echo "Homebrew is already intalled, updating it..."
-  brew update && brew upgrade
-  echo "Homebrew updated."
+  echo "Homebrew is already intalled."
 fi
 
 IS_HELPER_TOOLS_INSTALLED=$(thefuck --version  >/dev/null 2>&1)
-if [[ IS_HELPER_TOOLS_INSTALLED ]]; then
+if [[ $IS_HELPER_TOOLS_INSTALLED ]]; then
   echo "Intalling helper tools..."
   brew install fzf \
                thefuck
@@ -28,6 +26,18 @@ if [[ IS_HELPER_TOOLS_INSTALLED ]]; then
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   echo "Helper tools installed!"
+else
+  echo "Helper tools already intalled."
+fi
+
+echo "Check Dracula theme."
+DRACULA_THEME="$HOME/.dracula.zsh-theme"
+echo "DRACULA_THEME=$DRACULA_THEME"
+if [ ! -d $DRACULA_THEME ]; then
+  echo "There is no Dracula theme. Installing..."
+  git clone https://github.com/dracula/zsh.git $DRACULA_THEME
+  ln -s $DRACULA_THEME/dracula.zsh-theme $OH_MY_ZSH/themes/dracula.zsh-theme
+  echo "Dracula theme has been installed"
 fi
 
 echo "Restart ZSH configuration"
